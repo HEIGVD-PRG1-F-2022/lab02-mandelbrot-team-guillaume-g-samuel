38d367d54f6cd3e78253ee5e5d6181e3b276cc9c
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2022-10-05 20:18:19 +0200

    Implement wikipedia pseudocode in generate(x0, y0)

--- main.cpp
+++ main.cpp
@@ -1,3 +1,21 @@
 #include <iostream>
 
-int main(){return 0;}
+// Implementation of this pseudocode:
+// https://en.wikipedia.org/wiki/Mandelbrot_set#Computer_drawings
+void generate(int x0, int y0) {
+    double x = 0.0;
+    double y = 0.0;
+    int iteration = 0;
+    int max_iteration = 1000;
+    int xtemp;
+    while (x * x + y * y <= 2 * 2 && iteration < max_iteration) {
+        xtemp = x * x - y * y + x0;
+        y = 2 * x * y + y0;
+        x = xtemp;
+        iteration++;
+    }
+}
+
+int main() {
+    return 0;
+}
