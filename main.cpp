4c4183d9d4b35c3a1c92828c49a61266eaacfbaf
Guillaume Gonin <tux.caroline@gmail.com>
2022-10-06 13:46:11 +0200

    Pair Programming session 1, add displayArray, changed generate return and main adapatation

--- main.cpp
+++ main.cpp
@@ -5,23 +5,45 @@
  * Authors: Guillaume Gonin and Samuel Roland
  */
 #include <iostream>
+#include <vector>
+
+const int max_iteration = 1000;
 
 // Implementation of this pseudocode:
 // https://en.wikipedia.org/wiki/Mandelbrot_set#Computer_drawings
-void generate(int x0, int y0) {
+int generate(int x0, int y0) {
     double x = 0.0;
     double y = 0.0;
     int iteration = 0;
-    int max_iteration = 1000;
-    int xtemp;
+    int xTemp;
     while (x * x + y * y <= 2 * 2 && iteration < max_iteration) {
-        xtemp = x * x - y * y + x0;
+        xTemp = x * x - y * y + x0;
         y = 2 * x * y + y0;
-        x = xtemp;
+        x = xTemp;
         iteration++;
+    }
+    return iteration;
+}
+
+void displayArray(std::vector<std::vector<int>> array) {
+    for (int x = 0; x < array.size(); x++) {
+        for (int y = 0; y < array[x].size(); y++) {
+            std::cout << "["<< array[x][y] << "]";
+        }
+        std::cout << std::endl;
     }
 }
 
 int main() {
+    int width = 100, height = 100;
+    std::vector<std::vector<int>> array(width, std::vector<int>(height, 0));
+    for (int x = 0; x < array.size(); x++) {
+        for (int y = 0; y < array[x].size(); y++) {
+            array[x][y] = generate(x, y);
+        }
+    }
+
+    displayArray(array);
+
     return 0;
 }
